4c2323250beec6ad9105b4d420a9f72be4acecec
Estefi Turin <estefi.turin@gmail.com>
2024-05-21 21:24:56 -0300

    load

---
+++ src/test/java/com/swag/pe/pages/validation/ValidationPage.java
@@ -0,0 +1,24 @@
+package com.swag.pe.pages.validation;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+
+import java.util.List;
+
+public class ValidationPage extends PageObject {
+
+    @FindBy(xpath = "//div[@class='product_label']")
+    protected WebElementFacade lbl_product;
+
+    @FindBy(xpath = "//h3[@data-test=\"error\" and text()='Username and password do not match any user in this service']")
+    protected WebElementFacade lbl_errorMessage;
+
+    @FindBy(xpath = "//div[@class='inventory_item_name']")
+    protected List<WebElementFacade> productsList;
+
+    @FindBy(xpath = "//h2[text()='THANK YOU FOR YOUR ORDER']")
+    protected WebElementFacade lbl_order;
+
+
+}
---
+++ src/test/java/com/swag/pe/steps/validation/ValidationStep.java
@@ -0,0 +1,45 @@
+package com.swag.pe.steps.validation;
+
+import com.swag.pe.pages.validation.ValidationPage;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.Step;
+
+public class ValidationStep extends ValidationPage {
+    @Step("Validar visualizacion del modulo de productos")
+    public Boolean titleIsVisible() {
+        return lbl_product.isDisplayed();
+    }
+
+    @Step("Validar visualizacion del mensaje de error")
+    public Boolean errorMessageIsDisplayed() {
+        return lbl_errorMessage.isDisplayed();
+
+    }
+
+    @Step("Validar los productos listados en el carrito")
+    public Boolean productsAreDisplayed()  {
+        for(WebElementFacade product:productsList) {
+            if (product.isDisplayed()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Step("Validar que el carrito este vacio de productos")
+    public Boolean shoppingCartIsEmpty() {
+        for (WebElementFacade product:productsList){
+            if (!product.isDisplayed()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Step("Validar finalizacion de orden")
+    public Boolean orderTextIsDisplayed() {
+        return lbl_order.isDisplayed();
+    }
+
+
+}
